87b94d775634ecf0e0ac762a48357dd57c1809e9
Rahman <rahmanromli@yahoo.com>
2017-09-16 21:42:10 +0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	devtool: 'inline-source-map',
+	entry: './app/app.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+			{ test: /\.js$/, use: 'babel-loader', exclude: /(node_modules|bower_components)/ }
+		]
+	},
+	plugins: [new HtmlWebpackPlugin()]
+}
